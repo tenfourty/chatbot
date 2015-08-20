f8d1e03699894b100195ede96086aa173efe2bc3
Jeremy Brown <jeremy@tenfourty.com>
2015-08-20 16:47:43 +0200

    removed the start.sh script as we don't need it

--- start.sh
+++
@@ -1,9 +0,0 @@
-#!/bin/bash
-
-export HUBOT_IRC_SERVER="apigee.irc.slack.com"
-export HUBOT_IRC_NICK="chatbot"
-export HUBOT_IRC_ROOMS=$(cat channels)
-export HUBOT_IRC_USESSL="true"
-export HUBOT_IRC_UNFLOOD="false"
-
-bin/hubot -a irc
